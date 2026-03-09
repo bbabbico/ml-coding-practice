fef14a4c331ac20f948f5b2966d2b8c661f0ecaf
leedohyun <wonehgus1234@naver.com>
2026-03-09 18:21:31 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -420,4 +420,18 @@ f.close()
 # 파일에 내용 추가하기
 f = open('example.txt','w')
 for i in range(6,11):
-  data = '%d번쨰 줄 추가입니다. \n'%i
+  data = '%d번쨰 줄 추가입니다. \n' % i
+  f.write(data)
+f.close
+
+# 파일 모드 'r' - readline()
+f = open('example.txt', 'r')
+
+while True:
+  line = f.readline()
+  if not line: break
+  print(line)
+
+f.close()
+
+
