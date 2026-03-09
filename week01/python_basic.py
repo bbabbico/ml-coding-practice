d7e921a1a2390237841a59469179c5cde5b41ebb
leedohyun <wonehgus1234@naver.com>
2026-03-09 17:50:02 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -12,3 +12,33 @@ print(7%3)
 #나눗셈 몫 구하기
 print(a//b)
 print(7//3)
+
+s1="Hello Python"
+print(s1)
+
+s3 ='''Hello
+Python'''
+print(s3)
+
+head = "Python"
+tail =" is fun"
+
+# 문자열 곱하기
+print(head*2)
+print("="*5)
+
+# 문자열 인덱싱
+a="Now is better than never"
+print(a[0])
+print(a[4])
+print(a[-1])
+print(a[-2])
+
+# 문자열 슬라이싱
+b = a[0] + a[1] + a[2]
+print(b)
+
+print(a[4:6])
+print(a[4:6])
+print(a[4:6])
+print(a[4:6])
