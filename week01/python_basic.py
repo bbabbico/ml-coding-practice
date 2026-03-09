58b54de573d23208f864e14cef4b4c5b9850d965
leedohyun <wonehgus1234@naver.com>
2026-03-09 18:00:32 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -189,3 +189,17 @@ print(dic)
 del dic[1]
 print(dic)
 
+# 원소의 value 구하기
+print(dic['phone'])
+print(dic['name'])
+
+# key의 리스트 만들기
+print(dic.keys())
+print(list(dic.keys()))
+
+for key in dic.keys():
+  print(dic[key])
+
+# value의 리스트 만들기
+print(dic.values())
+print(list(dic.values()))
