863ebe63324a5e75f6da70b421f20d9bcd8a6399
leedohyun <wonehgus1234@naver.com>
2026-03-09 17:52:02 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -49,3 +49,16 @@ print(a.count('p'))
 
 # 문자 위치 확인
 print(a.find('y'))
+print(a.find('p'))
+print(a.index('y'))
+# print(a.index('p')) #오류발생
+
+# 문자 삽입
+b = ","
+c = b.join('Abcd')
+print(c)
+
+# 대소문자 변환
+print(a.upper())
+print(a.lower())
+
