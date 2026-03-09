a387c93f282b7d0762f562b1558ae579e96907c2
leedohyun <wonehgus1234@naver.com>
2026-03-09 18:22:31 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -434,4 +434,19 @@ while True:
 
 f.close()
 
+# 파일 모드 'r' - readlines()
+f = open('example.txt', 'r')
+lines = f.readlines()
+print(lines)
+
+for line in lines:
+  print(line)
+
+f.close()
+
+# 파일 모드 'r' - read()
+f = open('example.txt','r')
+data = f.read()
+f.close()
+data
 
