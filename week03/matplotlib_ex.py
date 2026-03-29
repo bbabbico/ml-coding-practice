f082ad81abf54790e7c5b0366a99f25256de1b92
leedohyun <wonehgus1234@naver.com>
2026-03-29 19:31:55 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -357,3 +357,9 @@ axes[1].set_xlabel('Parch')
 axes[1].set_ylabel('Survived Count', color='deeppink')
 axes[1].tick_params(axis='y', labelcolor='deeppink')
 axes[1].legend(loc='upper right')
+
+# 제목 설정
+fig.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titanic')
+fig.tight_layout()
+plt.savefig('Figure15.png')
+plt.close()
