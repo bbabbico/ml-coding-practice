0008b739f3a163cac6ded72988561d2a44b81b1a
leedohyun <wonehgus1234@naver.com>
2026-03-29 19:01:25 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -62,3 +62,9 @@ plt.close()
 # 성별에 따른 생존자의 수 계산
 survived_counts = titanic[titanic['Survived'] == 1]['Sex'].value_counts()
 print(survived_counts)
+
+# 수평 막대 그래프 그리기
+bars = plt.barh(survived_counts.index, survived_counts, color=['darkturquoise', 'salmon'])
+plt.title('Survived Counts by Gender on Titanic')
+plt.xlabel('Count')
+plt.ylabel('Gender')
