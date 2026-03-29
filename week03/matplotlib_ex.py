18d6a5f819837670bbbda0ae9f6d6b8786897cb4
leedohyun <wonehgus1234@naver.com>
2026-03-29 19:15:55 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -221,3 +221,6 @@ violin_plot = plt.violinplot([titanic[titanic['Pclass'] == 1]['Age'],
                               titanic[titanic['Pclass'] == 3]['Age']],
                              showmeans=False, showmedians=True)
 
+plt.title('Violin Plot of Age by Pclass')
+plt.xlabel('Pclass')
+plt.ylabel('Age')
