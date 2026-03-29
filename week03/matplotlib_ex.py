840f8a868894cc9e1b895e0b9c069fa6710a3570
leedohyun <wonehgus1234@naver.com>
2026-03-29 19:09:55 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -148,3 +148,11 @@ print(correlation_matrix)
 plt.matshow(correlation_matrix, cmap='PuRd_r')
 plt.colorbar()
 
+# x축과 y축의 눈금 설정
+plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotation=45)
+plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
+
+plt.title('Correlation Heatmap of Titanic')
+plt.savefig('Figure07.png')
+plt.close()
+
