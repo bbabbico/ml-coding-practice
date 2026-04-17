7c025a5d2e523b81adcff99aea86c7bf07cac3d7
leedohyun <wonehgus1234@naver.com>
2026-04-17 23:35:56 +0900

    Updated Changes

---
+++ week06/naverapi.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import urllib.request
+import datetime
+import json
+
+client_id = 'Client ID'
+client_secret = 'Client Secret'
+
+def main():
+
+    node = 'news'                                             # 크롤링할 대상
