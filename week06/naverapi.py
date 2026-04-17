e26d10f92f3e93b15a2d80ce2fd730aecb5c6c90
leedohyun <wonehgus1234@naver.com>
2026-04-17 23:36:56 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -9,3 +9,11 @@ client_secret = '4qspqALIYW'
 def main():
 
     node = 'news'                                             # 크롤링할 대상
+    srcText = input('검색어를 입력하세요: ')
+
+    cnt = 0
+    jsonResult = []
+
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
+    total = jsonResponse['total']
+
