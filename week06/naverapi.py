7865135d672342ea7176c948c3a6b04be78a43f0
leedohyun <wonehgus1234@naver.com>
2026-04-17 23:36:26 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -3,8 +3,8 @@ import urllib.request
 import datetime
 import json
 
-client_id = 'Client ID'
-client_secret = 'Client Secret'
+client_id = 'stGqeRCNS0NoogAMpS4p'
+client_secret = '4qspqALIYW'
 
 def main():
 
