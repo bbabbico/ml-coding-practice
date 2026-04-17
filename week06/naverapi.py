a15b25cf2496f07baedbe593ea555c7fbabdcaf0
leedohyun <wonehgus1234@naver.com>
2026-04-17 23:42:56 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -85,6 +85,12 @@ def getPostData(post, jsonResult, cnt):  #[CODE 3]
         'title': title,
         'description': description,
         'org_link': org_link,
+        'link': link,
+        'pDate': pDate
+    })
+    return
+
+main()
 
 
 
