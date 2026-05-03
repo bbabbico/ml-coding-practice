f5a8239cbb3126bed591314226570cca35bfa513
leedohyun <wonehgus1234@naver.com>
2026-05-03 19:58:27 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -31,3 +31,12 @@ for idx, image_data in enumerate(X[:100]):
     plot_digit(image_data)
 plt.subplots_adjust(wspace=0, hspace=0)
 plt.show()
+
+# train/test 데이터셋 나누기
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# 이진 분류기 훈련
+y_train_5 = (y_train == '5')  # 5는 True고, 다른 숫자는 모두 False
+y_test_5 = (y_test == '5')
+
+from sklearn.linear_model import SGDClassifier
