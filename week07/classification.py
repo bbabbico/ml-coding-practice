2dd20d42d44103bbc0157e0b9848bf2c68da9c24
leedohyun <wonehgus1234@naver.com>
2026-05-03 20:00:27 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -64,3 +64,13 @@ from sklearn.model_selection import cross_val_predict
 
 y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_train_5, y_train_pred)
+print(cm)
+
+y_train_perfect_predictions = y_train_5  # 완벽한 분류기일 경우
+print(confusion_matrix(y_train_5, y_train_perfect_predictions))
+
+# 정밀도와 재현율
+from sklearn.metrics import precision_score, recall_score
