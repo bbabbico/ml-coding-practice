2af65a06b2f6970a33eb742919acfd53fcfc662b
leedohyun <wonehgus1234@naver.com>
2026-05-05 21:23:57 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -42,3 +42,12 @@ netflix.dropna(axis = 0, inplace=True)
 # .info() : 열에 대한 요약 정보 확인
 # 8807 rows(원본 데이터 행 개수) - 17 rows(결측치 행) = 8790 rows(결측치가 제거된 행 개수)
 netflix.info()
+
+# 데이터프레임의 각 컬럼별 결측치 개수 반환
+# isna() == isnull() : 결측 값은 True 반환하고, 그 외에는 False 반환
+netflix.isna().sum()
+
+# 시청 등급 기준표를 참고하여 Netflix의 rating 변수를 이용한 age_group_dic 변수 생성
+# rating 컬럼의 값을 age_group이라는 새로운 컬럼으로 복사
+netflix['age_group'] = netflix['rating']
+
